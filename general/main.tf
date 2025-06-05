fc19ef6922baf59a886d4fbaafb519f40c845e02
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-05 08:20:01 -0500

    removed default rules in general folder

--- general/main.tf
+++ general/main.tf
@@ -80,35 +80,3 @@ resource "aws_security_group" "security_group" {
         Name = "${var.type}-security-group"
     }
 }
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.security_group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.security_group.id
-  cidr_ipv6         = "::/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_ipv4" {
-  security_group_id = aws_security_group.security_group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 0
-  to_port           = 0
-  ip_protocol       = "-1"
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_ipv6" {
-  security_group_id = aws_security_group.security_group.id
-  cidr_ipv6         = "::/0"
-  from_port         = 0
-  to_port           = 0
-  ip_protocol       = "-1"
-}
