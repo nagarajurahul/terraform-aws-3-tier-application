e810e85c8a5b769bab967c296e613067131439db
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-04 20:18:22 -0500

    defined egress rules

--- main.tf
+++ main.tf
@@ -88,3 +88,19 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   ip_protocol       = "tcp"
   to_port           = 443
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_ipv4" {
+  security_group_id = aws_security_group.frontend.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_ipv6" {
+  security_group_id = aws_security_group.frontend.id
+  cidr_ipv6         = "::/0"
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1"
+}
