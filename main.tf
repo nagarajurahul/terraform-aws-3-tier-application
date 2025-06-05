8928d84b11698fc80860c08867d573e1f05c359e
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-04 21:03:49 -0500

    attach security group to launch template

--- main.tf
+++ main.tf
@@ -26,6 +26,8 @@ resource "aws_launch_template" "frontend"{
 
     instance_type = var.instance_type
 
+    vpc_security_group_ids = [aws_security_group.frontend]
+
     tag_specifications {
         resource_type = "instance"
 
