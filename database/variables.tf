41cc23262a135e8f3eb24dd7e79516ed2f2f464d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-05 23:40:53 -0500

    removed password restriction

--- database/variables.tf
+++ database/variables.tf
@@ -2,21 +2,17 @@ variable "username" {
 
 }
 variable "password"{
-    type      = string
-    ephemeral = true
+    
 }
-
 variable "instance_class"{
     default = "db.t3.micro"
 }
 variable "allocated_storage"{
     default = 10
 }
-
 variable "db_name" {
     default = "my_db"
 }
-
 variable "subnet_ids"{
     type    = list(string)
 }
