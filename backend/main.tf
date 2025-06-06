3b64431bffd7af1ed4e88ab379078f1ab2d63034
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-05 23:08:17 -0500

    modified backend main too

--- backend/main.tf
+++ backend/main.tf
@@ -1,11 +1,24 @@
+module "security_group"{
+    source = "../security-group"
+
+    type = "backend"
+    vpc_id = var.vpc_id
+}
+
+module "security_group_rules"{
+    source = "../security-group-rules/backend"
+
+    security_group_id = module.security_group.security_group_id
+}
+
 module "instance_with_template"{
     source = "../instance-with-template"
 
     owner = var.owner
     env=var.env
     type = "backend"
-
-    vpc_id = var.vpc_id 
     
+    vpc_id = var.vpc_id
+    security_group_id = module.security_group.security_group_id
 
 }
