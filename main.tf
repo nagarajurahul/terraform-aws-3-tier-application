1e919e07e6d7bbfcfcb1a4bc54a8d904ccdd2fb6
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-05 23:32:48 -0500

    created root module

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "frontend" {
+    source = "./frontend"
+
+    owner = var.owner
+    env=var.env
+    vpc_id = var.vpc_id
+}
+
+module "backend" {
+    source = "./frontend"
+    
+    owner = var.owner
+    env=var.env
+    vpc_id = var.vpc_id
+}
