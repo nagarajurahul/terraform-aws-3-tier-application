724c7ffdff40410ae65fcd65d35d966ac72d9580
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-10 23:32:25 -0500

    removed other outputs

--- output.tf
+++ output.tf
@@ -1,26 +1,26 @@
-output "frontend_instance_id"{
-    value = module.frontend.frontend_instance_id
+# output "frontend_instance_id"{
+#     value = module.frontend.frontend_instance_id
 
-    description = "Frontend - Instance ID of the EC2"
-}
+#     description = "Frontend - Instance ID of the EC2"
+# }
 
-output "frontend_security_group_id"{
-    value = module.frontend.frontend_security_group_id
+# output "frontend_security_group_id"{
+#     value = module.frontend.frontend_security_group_id
     
-    description = "Frontend - Security group id attached to the instance"
-}
+#     description = "Frontend - Security group id attached to the instance"
+# }
 
-output "backend_instance_id"{
-    value = module.backend.backend_instance_id
+# output "backend_instance_id"{
+#     value = module.backend.backend_instance_id
 
-    description = "Backend - Instance ID of the EC2"
-}
+#     description = "Backend - Instance ID of the EC2"
+# }
 
-output "backend_security_group_id"{
-    value = module.backend.backend_security_group_id
+# output "backend_security_group_id"{
+#     value = module.backend.backend_security_group_id
     
-    description = "Backend - Security group id attached to the instance"
-}
+#     description = "Backend - Security group id attached to the instance"
+# }
 
 output "rds_endpoint" {
   value     = module.database.rds_endpoint
