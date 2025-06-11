7295e1ae040ec2bb5dc125498ec0851f3c2970df
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-10 22:55:01 -0500

    added frontend outputs to root module

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "frontend_instance_id"{
+    value = module.frontend.frontend_instance_id
+
+    description = "Frontend - Instance ID of the EC2"
+}
+
+output "frontend_security_group_id"{
+    value = module.frontend.frontend_security_group_id
+    
+    description = "Frontend - Security group id attached to the instance"
+}
