ec34b132169a8f2e44b9ad0231f5cadab332556b
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-10 22:55:43 -0500

    added backend outputs to root module

--- output.tf
+++ output.tf
@@ -9,3 +9,15 @@ output "frontend_security_group_id"{
     
     description = "Frontend - Security group id attached to the instance"
 }
+
+output "backend_instance_id"{
+    value = module.backend.backend_instance_id
+
+    description = "Backend - Instance ID of the EC2"
+}
+
+output "backend_security_group_id"{
+    value = module.backend.backend_security_group_id
+    
+    description = "Backend - Security group id attached to the instance"
+}
